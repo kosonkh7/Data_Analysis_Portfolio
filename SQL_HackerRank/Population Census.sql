4f81d979e7c71f71c06fb9c512f3e7747941d255
kosonkh7 <83086978+kosonkh7@users.noreply.github.com>
2023-11-22 14:09:35 +0900

    Update Population Census.sql

--- SQL_HackerRank/Population Census.sql
+++ SQL_HackerRank/Population Census.sql
@@ -1,3 +1,5 @@
+-- https://www.hackerrank.com/challenges/asian-population/problem?h_r=internal-search
+
 SELECT SUM(CITY.POPULATION)
 FROM CITY
     INNER JOIN COUNTRY ON CITY.COUNTRYCODE = COUNTRY.CODE
