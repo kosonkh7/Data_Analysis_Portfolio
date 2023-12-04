aa8f4403b751a7b05c3b390ddfaa229990c939ce
kosonkh7 <83086978+kosonkh7@users.noreply.github.com>
2023-12-04 18:22:46 +0900

    Create Placements.sql

---
+++ SQL_HackerRank/Placements.sql
@@ -0,0 +1,9 @@
+https://www.hackerrank.com/challenges/placements/problem?h_r=internal-search
+
+SELECT Students.Name
+FROM Friends
+    LEFT JOIN Students ON Friends.ID = Students.ID
+    LEFT JOIN Packages AS s ON Friends.ID = s.ID
+    LEFT JOIN Packages AS f ON Friends.Friend_ID = f.ID
+WHERE s.Salary < f.Salary
+ORDER BY f.Salary 
