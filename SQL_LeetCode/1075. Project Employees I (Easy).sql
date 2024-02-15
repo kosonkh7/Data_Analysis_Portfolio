01e44ce56a3e84b86d2e17457edd620d38aa3e16
kosonkh7 <83086978+kosonkh7@users.noreply.github.com>
2024-02-15 18:03:12 +0900

    Create 1075. Project Employees I (Easy).sql

---
+++ SQL_LeetCode/1075. Project Employees I (Easy).sql
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/project-employees-i
+
+SELECT project_id, ROUND(AVG(experience_years), 2) AS average_years
+FROM Project 
+LEFT JOIN Employee ON Project.employee_id = Employee.employee_id
+GROUP BY project_id
