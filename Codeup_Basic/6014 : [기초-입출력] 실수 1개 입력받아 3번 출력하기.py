5424e7cc05494cf1157d4286e5c1e99d42c88ea0
kosonkh7 <83086978+kosonkh7@users.noreply.github.com>
2024-04-23 21:43:39 +0900

    Create 6014 : [기초-입출력] 실수 1개 입력받아 3번 출력하기.py

---
+++ Codeup_Basic/6014 : [기초-입출력] 실수 1개 입력받아 3번 출력하기.py
@@ -0,0 +1,17 @@
+"""
+실수(real number) 1개를 입력받아 줄을 바꿔 3번 출력해보자.
+
+예시
+...
+print(f)  #f에 저장되어있는 값을 출력하고 줄을 바꾼다.
+print(f)
+print(f)
+와 같은 방법으로 3번 줄을 바꿔 출력할 수 있다.
+
+"""
+
+
+a = input()
+print(a)
+print(a)
+print(a)
