b2f200fac6ac9a563685ba305c076edfa0b181b8
kosonkh7 <83086978+kosonkh7@users.noreply.github.com>
2024-05-14 05:02:00 +0900

    Create 6095 : [기초-리스트] 바둑판에 흰 돌 놓기.py

---
+++ Codeup_Basic/6095 : [기초-리스트] 바둑판에 흰 돌 놓기.py
@@ -0,0 +1,13 @@
+pan = [[0]* 19 for _ in range(19)]
+
+n = int(input())
+
+for i in range(n):
+    x, y = map(int, input().split())
+    pan[x-1][y-1] = 1
+
+
+for i in range(19):
+    for j in range(19):
+        print(pan[i][j], end=' ')
+    print()
